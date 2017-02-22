df2c3410041b3b76c1b6a30613f70eae7005a5e0
andrey-bgm <andrey.bgm@gmail.com>
2017-02-22 03:59:13 +0300

    Step #4

---
+++ src/bin/brain-even.js
@@ -0,0 +1,35 @@
+#! /usr/bin/env node
+import readlineSync from 'readline-sync';
+
+const step = (numbers, name) => {
+  if (numbers.length === 0) {
+    console.log(`Congratulations, ${name}!`);
+    return;
+  }
+
+  const number = numbers[0];
+  const expected = number % 2 === 0 ? 'yes' : 'no';
+
+  console.log(`Question: ${number}`);
+  const answer = readlineSync.question('Your answer: ');
+
+  if (answer === expected) {
+    console.log('Correct!');
+    step(numbers.slice(1), name);
+  } else {
+    console.log(`'${answer}' is a wrong answer ;(. The correct answer was '${expected}'.`);
+    console.log(`Let's try again, ${name}!`);
+  }
+};
+
+const startGame = () => {
+  console.log('Welcome to the Brain Games!');
+  console.log('Answer "yes" if a number is even, otherwise answer "no".\n');
+
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!\n`);
+
+  step([15, 6, 15], name);
+};
+
+startGame();
